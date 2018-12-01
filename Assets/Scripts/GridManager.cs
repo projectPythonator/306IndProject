4c1a4e863c1b0a723bb1529dd8417efc62822775
Agis Daniels <ard902@usask.ca>
2018-12-01 10:05:54 -0600

    added in the game to play

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        GameObject tmp = PoolerScript.current.GetPooledObject();
+    public int gridSize = 23;
+    public float tileSize = 34.0f/100.0f;
+    public GameObject[,] Grid;
+    public int[,] GridSpaces;
+    // Use this for initialization
+    void Start () {
+        Grid = new GameObject[gridSize, gridSize];
+        GridSpaces = new int[gridSize, gridSize];
 
-        while(tmp != null)
+        float x = 0, y = 0;
+
+
+        for (int i = 0; i < gridSize; i++)
         {
-            tmp.SetActive(true);
-            tmp = PoolerScript.current.GetPooledObject();
+            for (int j = 0; j < gridSize; j++)
+            {
+                GameObject tmp = PoolerScript.current.GetPooledObject();
+                tmp.SetActive(true);
+                tmp.transform.position = new Vector3(x, y, 0);
+                Grid[i, j] = tmp;
+                GridSpaces[i, j] = 1;
+                x += tileSize;
+            }
+            y += tileSize;
+            x = 0;
         }
-
     }
 	
+
 	// Update is called once per frame
 	void Update () {
 		
